c9d212fca2492b313da6c532802e1142a74fc62f
Jack Young <jackyoung@lost.parts>
2020-07-11 21:01:44 -0400

    update

--- src/main/kotlin/parts/lost/mc/scriptexecutor/kotlin/commands/commandscriptexecute/SubCommandList.kt
+++ src/main/kotlin/parts/lost/mc/scriptexecutor/kotlin/commands/commandscriptexecute/SubCommandList.kt
@@ -1,11 +1,19 @@
 package parts.lost.mc.scriptexecutor.kotlin.commands.commandscriptexecute
 
+import org.bukkit.ChatColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import parts.lost.mc.scriptexecutor.kotlin.config.ConfigManager
 
 object SubCommandList: CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        TODO("Need to finish list command")
+
+        if (args.isNotEmpty())
+            sender.sendMessage("${ChatColor.RED}This subcommand does not take any arguments.")
+        else
+            sender.sendMessage("Available scripts: ${ConfigManager.getScripts().joinToString(" ") { it.first }}")
+
+        return true
     }
 }
