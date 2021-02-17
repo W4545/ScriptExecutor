198dfc0981f6fdef8dcb8c9b30bfc2f68f9b0d10
Jack Young <jackyoung@lost.parts>
2021-02-16 23:11:49 -0500

    More Automation work

--- src/main/kotlin/parts/lost/mc/scriptexecutor/kotlin/commands/commandseautomation/Create.kt
+++ src/main/kotlin/parts/lost/mc/scriptexecutor/kotlin/commands/commandseautomation/Create.kt
@@ -1,5 +1,6 @@
 package parts.lost.mc.scriptexecutor.kotlin.commands.commandseautomation
 
+import org.bukkit.ChatColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import parts.lost.mc.scriptexecutor.kotlin.config.ConfigManager
@@ -9,6 +10,10 @@ import parts.lost.mc.scriptexecutor.kotlin.interfaces.SubCommand
 
 object Create: SubCommand {
     private val digits = arrayOf('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
+
+    private val dateRegex = """\d{4}-\d{1,2}-\d{1,2}""".toRegex()
+    private val timeRegex = """\d{1,2}:\d{2}""".toRegex()
+    private val timeLengthRegex = """\d+[smhd]""".toRegex()
 
     override val name = "create"
     override val helpNotes = object : HelpNotes {
@@ -20,11 +25,11 @@ object Create: SubCommand {
         ): Boolean {
             sender.sendMessage("Description: " +
                     "Creates a new automation request with the given delay or datetime, and optional period")
-            sender.sendMessage("Examples: /$label create hello 1m 10s " +
+            sender.sendMessage("Examples: /$label create hello console 1m 10s " +
                     "(create an automation script that runs every 10 seconds starting in 1 minute.)")
-            sender.sendMessage("Examples: /$label create hello 2021-12-1 14:30 " +
+            sender.sendMessage("Examples: /$label create hello default 2021-12-1 14:30 " +
                     "(creates an automation script that runs on the date and time provided)")
-            sender.sendMessage("Usage: /$label create <script> <delay or date> <optional time> <optional period>")
+            sender.sendMessage("Usage: /$label create <script> <script configuration> <delay or date> <optional time> <optional period>")
 
             return true
         }
@@ -32,7 +37,27 @@ object Create: SubCommand {
     }
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        TODO("Not yet implemented")
+        val scriptConfiguration = ConfigManager.getScript(args[0], args[1])
+
+        sender.sendMessage(scriptConfiguration?.verbose ?: "${ChatColor.RED} Unable to find or infer configuration.")
+
+        val date = dateRegex.matchEntire(args[2])?.value
+        val delay = timeLengthRegex.matchEntire(args[2])?.value
+        val time = timeRegex.matchEntire(args[3])?.value
+        val period = if (time == null)
+            timeLengthRegex.matchEntire(args[3])?.value
+        else
+            timeLengthRegex.matchEntire(args[4])?.value
+
+        if (date != null && time != null) {
+            sender.sendMessage("")
+        } else if (delay != null) {
+            //TODO
+        } else
+            sender.sendMessage("${ChatColor.RED}Unknown arguments provided. " +
+                    "See ${ChatColor.DARK_RED}/$label help create${ChatColor.RESET}${ChatColor.RED} for details")
+
+        return true
     }
 
     override fun onTabComplete(
@@ -43,25 +68,32 @@ object Create: SubCommand {
     ): MutableList<String> {
         return when (args.size) {
             2 -> ConfigManager.getScriptNames().toMutableList()
-            3 -> {
-                if (args[2].endsWith(digits)) {
-                    if (args[2].contains('-')) {
-                        if (args[2].count { it == '-' } < 2)
-                            mutableListOf(args[2] + "-")
+            3 -> ConfigManager.getScriptSchemeConfigurations(args[1]).toMutableList()
+            4 -> {
+                if (args[3].endsWith(digits)) {
+                    if (args[3].contains('-')) {
+                        if (args[3].count { it == '-' } < 2)
+                            mutableListOf(args[3] + "-")
                         else
                             MutableList(0) { "" }
                     } else {
-                        if (args[2].all { it.isDigit() } && args[2].length == 4)
-                            arrayOf("-", "s", "m", "h", "d").map { args[2] + it }.toMutableList()
+                        if (args[3].all { it.isDigit() } && args[3].length == 4)
+                            arrayOf("-", "s", "m", "h", "d").map { args[3] + it }.toMutableList()
                         else
-                            arrayOf("s", "m", "h", "d").map { args[2] + it }.toMutableList()
+                            arrayOf("s", "m", "h", "d").map { args[3] + it }.toMutableList()
                     }
                 }
                 else MutableList(0) { "" }
             }
-            4 -> {
-                if (args[3].endsWith(digits))
-                    arrayOf("-", "s", "m", "h", "d").map { args[3] + it }.toMutableList()
+            5 -> {
+                if (args[4].endsWith(digits) && args[4].count { it == ':' } == 0)
+                    arrayOf("-", "s", "m", "h", "d", ":").map { args[4] + it }.toMutableList()
+                else
+                    MutableList(0) { "" }
+            }
+            6 -> {
+                if (args[5].endsWith(digits))
+                    arrayOf("s", "m", "h", "d").map { args[5] + it }.toMutableList()
                 else
                     MutableList(0) { "" }
             }
