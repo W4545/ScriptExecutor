bbc04ae4fb643de3c91ec9986c12203244a5fad7
Jack Young <jackyoung@lost.parts>
2021-02-17 17:08:20 -0500

    Updated scheduler to help handle deleting AutomatedScript references after script completion

--- src/main/kotlin/parts/lost/mc/scriptexecutor/kotlin/automation/Scheduler.kt
+++ src/main/kotlin/parts/lost/mc/scriptexecutor/kotlin/automation/Scheduler.kt
@@ -3,15 +3,17 @@ package parts.lost.mc.scriptexecutor.kotlin.automation
 import org.bukkit.scheduler.BukkitTask
 import parts.lost.mc.scriptexecutor.kotlin.ScriptExecutor
 import parts.lost.mc.scriptexecutor.kotlin.config.ScriptConfiguration
+import parts.lost.mc.scriptexecutor.kotlin.exceptions.ScriptCancelException
 import parts.lost.mc.scriptexecutor.kotlin.scriptrunner.CreateScript
 import parts.lost.mc.scriptexecutor.kotlin.storage.AutomatedScript
 import parts.lost.mc.scriptexecutor.kotlin.storage.Storage
+import java.time.Instant
 import java.util.*
 
 object Scheduler {
 
     fun schedule(scriptConfiguration: ScriptConfiguration, date: Date) : AutomatedScript {
-        val time: Long = System.currentTimeMillis() - date.time
+        val time: Long = date.time - Date.from(Instant.now()).time
 
         return if (time < 0)
             schedule(scriptConfiguration, 0L)
@@ -19,30 +21,44 @@ object Scheduler {
             schedule(scriptConfiguration, time / 1000 * 20)
     }
 
+    fun schedule(scriptConfiguration: ScriptConfiguration, date: Date, interval: Long) : AutomatedScript {
+        val time: Long = date.time - Date.from(Instant.now()).time
+
+        return if (time < 0)
+            schedule(scriptConfiguration, 0L, interval)
+        else
+            schedule(scriptConfiguration, time / 1000 * 20, interval)
+    }
+
     fun schedule(scriptConfiguration: ScriptConfiguration, delay: Long, period: Long)
-    : AutomatedScript = schedule(scriptConfiguration) {
+    : AutomatedScript = schedule(scriptConfiguration, false) {
         ScriptExecutor.plugin.server.scheduler.runTaskTimer(ScriptExecutor.plugin, it, delay, period)
     }
 
-    fun schedule(scriptConfiguration: ScriptConfiguration, delay: Long) : AutomatedScript = schedule(scriptConfiguration) {
+    fun schedule(scriptConfiguration: ScriptConfiguration, delay: Long) : AutomatedScript = schedule(scriptConfiguration, true) {
         ScriptExecutor.plugin.server.scheduler.runTaskLater(ScriptExecutor.plugin, it, delay)
     }
 
-    private fun schedule(scriptConfiguration: ScriptConfiguration, initializer: (Runnable) -> BukkitTask) : AutomatedScript {
-        val runnable: () -> Unit = {
+    private fun schedule(scriptConfiguration: ScriptConfiguration, deleteOnCompletion: Boolean, initializer: (Runnable) -> BukkitTask) : AutomatedScript {
+        val scriptID = Storage.automatedScriptID(scriptConfiguration.name)
+        scriptConfiguration.additionalConfigurations["automated"] = "true"
+        scriptConfiguration.additionalConfigurations["automatedScriptID"] = scriptID
+        scriptConfiguration.additionalConfigurations["automateDelete"] = deleteOnCompletion.toString()
+
+        val automatedScript = AutomatedScript(scriptID, scriptConfiguration, null, deleteOnCompletion)
+        Storage.automatedScripts.add(automatedScript)
+
+        val bukkitTask = initializer {
             CreateScript.create(scriptConfiguration)
         }
 
-        val bukkitTask = initializer(runnable)
-
-        val automatedScript = AutomatedScript(Storage.automatedScriptID(scriptConfiguration.name), scriptConfiguration, bukkitTask)
-
-        Storage.automatedScripts.add(automatedScript)
+        automatedScript.bukkitTask = bukkitTask
 
         return automatedScript
     }
 
     fun cancel(automatedScript: AutomatedScript) {
-        automatedScript.bukkitTask.cancel()
+        automatedScript.bukkitTask?.cancel() ?: throw ScriptCancelException()
+        Storage.automatedScripts.remove(automatedScript)
     }
 }
