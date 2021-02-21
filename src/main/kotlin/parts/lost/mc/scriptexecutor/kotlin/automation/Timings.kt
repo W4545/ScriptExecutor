2ee2c3b26bf5034117ed4083ab38ced9887f422c
jacky <jackyoung@lost.parts>
2021-02-21 01:18:12 -0500

    More work on automation

--- src/main/kotlin/parts/lost/mc/scriptexecutor/kotlin/automation/Timings.kt
+++ src/main/kotlin/parts/lost/mc/scriptexecutor/kotlin/automation/Timings.kt
@@ -1,29 +1,47 @@
 package parts.lost.mc.scriptexecutor.kotlin.automation
 
 import org.bukkit.configuration.file.FileConfiguration
-import java.time.Instant
-import java.util.Date
+import parts.lost.mc.scriptexecutor.kotlin.config.ConfigManager
+import java.text.SimpleDateFormat
+import java.util.*
 
 
-interface Timing {
-    val period: Long
-    val delay: Long
-}
 
-class DateTiming(val date: Date, override val period: Long = 0) : Timing {
-    override val delay: Long
-        get() = date.time - Date.from(Instant.now()).time
-}
-
-class BasicTiming(override val period: Long, override val delay: Long) : Timing
+data class Timing(val delay: Long = 0L, val period: Long = 0L, val date: Date? = null)
 
 object Timings {
+
+    private val dateFormatter = SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
+
+    init {
+        dateFormatter.timeZone = if (ConfigManager.timeZoneOverride == null)
+            TimeZone.getDefault()
+        else
+            TimeZone.getTimeZone(ConfigManager.timeZoneOverride)
+    }
+
     fun factory(file: FileConfiguration): Timing {
-        val date = file.getString("date")
-        if (date != null)  {
+        return if (file.contains("date"))  {
+            val rawDate = file.getString("date")
+            val period = file.getLong("period")
 
+            val date = dateFormatter.parse(rawDate)
+
+            Timing(period = period, date = date)
+        } else {
+            val delay = file.getLong("delay")
+            val period = file.getLong("period")
+
+            Timing(delay, period)
         }
+    }
 
-        TODO()
+    fun write(file: FileConfiguration, timing: Timing) {
+        if (timing.date == null)
+            file["delay"] = timing.delay.toString()
+        else
+            file["date"] = dateFormatter.format(timing.date)
+
+        file["period"] = timing.period.toString()
     }
 }
