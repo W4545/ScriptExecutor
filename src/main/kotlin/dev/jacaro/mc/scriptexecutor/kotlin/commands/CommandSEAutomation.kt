463cdfb94512697785c9213c2319308bd846fa01
W4545 <W4545@jacaro.dev>
2021-08-22 05:06:56 +0000

    Activated nextrun command

--- src/main/kotlin/dev/jacaro/mc/scriptexecutor/kotlin/commands/CommandSEAutomation.kt
+++ src/main/kotlin/dev/jacaro/mc/scriptexecutor/kotlin/commands/CommandSEAutomation.kt
@@ -3,12 +3,13 @@ package dev.jacaro.mc.scriptexecutor.kotlin.commands
 import dev.jacaro.mc.scriptexecutor.kotlin.commands.commandseautomation.CancelAutomation
 import dev.jacaro.mc.scriptexecutor.kotlin.commands.commandseautomation.AutomatedScriptList
 import dev.jacaro.mc.scriptexecutor.kotlin.commands.commandseautomation.DebugAutomation
+import dev.jacaro.mc.scriptexecutor.kotlin.commands.commandseautomation.NextRun
 import dev.jacaro.mc.scriptexecutor.kotlin.commands.commandseautomation.create.Create
 import dev.jacaro.mc.scriptexecutor.kotlin.constructs.CommandManager
 import dev.jacaro.mc.scriptexecutor.kotlin.constructs.HelpCommand
 
 object CommandSEAutomation : CommandManager() {
     override val helpNotes = HelpCommand(this)
-    override val subCommands = listOf(Create, helpNotes, AutomatedScriptList, CancelAutomation, DebugAutomation)
+    override val subCommands = listOf(Create, helpNotes, AutomatedScriptList, CancelAutomation, DebugAutomation, NextRun)
     override val name = "seautomate"
 }
